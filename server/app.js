82afa7dd2a7cf733b31d6f5d5df9af569877ac1a
EdwardHoward <EdwardHoward@users.noreply.github.com>
2021-07-01 11:17:27 -0600

    Update app.js
    Add /animals endpoint

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 
 const app = express();
 
+const data = require('../data');
+
 app.use(cors('localhost'));
 app.use(logger('dev'));
 app.use(express.json());
@@ -12,5 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 
 /* Implement your routes here */
 
+app.get('/animals', (req, res) => {
+  res.json(data);
+});
 
 module.exports = app;
